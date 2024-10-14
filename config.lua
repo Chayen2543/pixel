9d8d1330db6eb24331e56faf25e04acaec8494da
Chayen2543 <Chayenmir4@gmail.com>
2024-10-15 02:53:35 +0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,14 @@
+shared.Configs = {
+    Enabled = true,
+    RedeemCode = true,
+    Speed = 2,  
+    GithubUrl = '',
+    GithubConfigUrl = '',
+    JobID = '',
+    AutoTrading = {
+    TradingMode = false,
+        ['Enabled'] = false,
+        ['Main User'] = '',
+        ['JobID'] = '',
+    },
+};
